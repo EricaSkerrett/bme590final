3ab732a8cbe7567c45cd2cc3375ca40c95e5b9b9
Roujia Wang <rosesharpaywang@RoujiamatoMacBook-Pro.local>
2018-12-07 22:08:20 -0500

    Display Image and Add Buttons for Window 4

--- gui.py
+++ gui.py
@@ -1,8 +1,9 @@
 import sys
 import os
-from PyQt5.QtWidgets import QMainWindow, QPushButton, \
-    QApplication, QInputDialog, QLineEdit, QLabel,\
-    QFileDialog, QTextEdit
+from PyQt5.QtWidgets import QMainWindow, QPushButton,\
+    QApplication, QInputDialog, QLineEdit, QLabel, \
+    QFileDialog, QTextEdit, QSpinBox, QVBoxLayout,\
+    QComboBox, QGroupBox, QFormLayout
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QIcon, QPixmap
 
@@ -111,8 +112,8 @@ class App3(QMainWindow):
         self.top = 10
         self.width = 640
         self.height = 400
-        self.next = None
         self.path, self.filename = os.path.split(filename[0])
+        self.next = None
         self.init_gui()
 
     def init_gui(self):
@@ -133,56 +134,79 @@ class App3(QMainWindow):
         button.setMinimumSize(200, 40)
         button.setToolTip('This is an example button')
         button.move(220, 300)
-        button.clicked.connect(self.print_text)
+        button.clicked.connect(self.next_window)
 
-    def print_text(self):
-        print("button clicked")
-        self.next = App4()
+    def next_window(self):
+        self.next = App4(self.path, self.filename)
+    # place holder for zip file scroll down menu
+    # def list_image(self)
+
+
+class App4(QMainWindow):
+
+    def __init__(self, path="", filename=""):
+        super().__init__()
+        self.title = 'Image Processor'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 400
+        self.next = None
+        self.path = path
+        self.filename = filename
+        self.init_gui()
+
+    def init_gui(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.statusBar().showMessage('Step 3: Process Image(s)!')
+        # Create widget
+        label = QLabel(self)
+        pixmap = QPixmap(os.path.join(self.path, self.filename))
+        pixmap2 = pixmap.scaledToWidth(250)
+        label.setPixmap(pixmap2)
+        label.setGeometry(50, 20, 640, 280)
+        self.button1()
+        self.button2()
+        self.button3()
+        self.button4()
+        self.show()
+
+    def button1(self):
+        button = QPushButton('Histogram Equalization', self)
+        button.setMinimumSize(200, 40)
+        button.setToolTip('This is an example button')
+        button.move(360, 60)
+        button.clicked.connect(self.on_click)
+
+    def button2(self):
+        button = QPushButton('Contrast Stretching', self)
+        button.setMinimumSize(200, 40)
+        button.setToolTip('This is an example button')
+        button.move(360, 120)
+        button.clicked.connect(self.on_click)
+
+    def button3(self):
+        button = QPushButton('Log Compression', self)
+        button.setMinimumSize(200, 40)
+        button.setToolTip('This is an example button')
+        button.move(360, 180)
+        button.clicked.connect(self.on_click)
+
+    def button4(self):
+        button = QPushButton('Reverse Video', self)
+        button.setMinimumSize(200, 40)
+        button.setToolTip('This is an example button')
+        button.move(360, 240)
+        button.clicked.connect(self.on_click)
+
+    @pyqtSlot()
+    def on_click(self):
+        print('PyQt5 button click')
         # place holder for get and post request
 
     # place holder for zip file scroll down menu
     # def list_image(self)
-
-    class App3(QMainWindow):
-
-        def __init__(self, filename=""):
-            super().__init__()
-            self.title = 'Image Processor'
-            self.left = 10
-            self.top = 10
-            self.width = 640
-            self.height = 400
-            self.next = None
-            self.path, self.filename = os.path.split(filename[0])
-            self.init_gui()
-
-        def init_gui(self):
-            self.setWindowTitle(self.title)
-            self.setGeometry(self.left, self.top, self.width, self.height)
-            self.statusBar().showMessage('Step 2: Upload Image(s)!')
-            # Create widget
-            label = QLabel(self)
-            pixmap = QPixmap(os.path.join(self.path, self.filename))
-            pixmap2 = pixmap.scaledToWidth(400)
-            label.setPixmap(pixmap2)
-            label.setGeometry(120, 20, 640, 280)
-            self.button_upload()
-            self.show()
-
-        def button_upload(self):
-            button = QPushButton('Upload Image', self)
-            button.setMinimumSize(200, 40)
-            button.setToolTip('This is an example button')
-            button.move(220, 300)
-            button.clicked.connect(self.print_text)
-
-        def print_text(self):
-            print("button clicked")
-            self.next = App4()
-            # place holder for get and post request
-
-        # place holder for zip file scroll down menu
-        # def list_image(self)
 
 
 if __name__ == '__main__':
